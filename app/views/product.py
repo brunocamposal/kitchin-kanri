413aa65b88506554885f5440a46cd8811ec79ea5
Willian <willian.bruschh@gmail.com>
2021-01-13 23:12:10 -0300

    chore: product table and schema

--- app/views/product.py
+++ app/views/product.py
@@ -7,3 +7,25 @@ bp_products = Blueprint('api_products', __name__, url_prefix='/products')
 
 
 @bp_products.route('/')
+def list_all():
+    pass
+
+
+@bp_products.route('/<int:product_id>')
+def list_one_product():
+    pass
+
+
+@bp_products.route('/', methods=['POST'])
+def create_one_product():
+    pass
+
+
+@bp_products.route('/<int:product_id>', methods=['DELETE'])
+def delete_one_product(user_id: int):
+    pass
+
+
+@bp_products.route('/<int:product_id>', methods=['PUT'])
+def update_one_product(user_id: int):
+    pass
