27a87c5c15a64e2297780bae9df9d67a2709bf11
brunocamposalves <brunocamposalves@hotmail.com>
2021-01-20 18:14:51 -0300

    feature/18: create product templates

--- app/views/front_end.py
+++ app/views/front_end.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, render_template
 from app.models import Category, Product
 import json
 
+
 bp_front_end = Blueprint('api_front_end', __name__, url_prefix='/')
 
 
@@ -13,7 +14,12 @@ def home():
 @bp_front_end.route('category_list', methods=['GET'])
 def list_category():
     response = Category.query.all()
-    category_list = [dict(id=category.id,name=category.name, link=f"category_list/{category.id}") for category in response]
+    category_list = [
+        dict(
+        id=category.id,
+        name=category.name, 
+        link=f"category_list/{category.id}") 
+        for category in response]
 
     return render_template("category_list.html.jinja", data=category_list)
 
@@ -21,6 +27,33 @@ def list_category():
 @bp_front_end.route('category_list/<int:category_id>', methods=['GET'])
 def list_category_id(category_id):
     response = Product.query.filter_by(category_id=category_id)
-    product_list = [dict(id=product.id, name=product.name, image=product.image, price=product.price) for product in response]
+    product_list = [
+        dict(
+        id=product.id, 
+        name=product.name, 
+        image=product.image, 
+        price=product.price, 
+        link=f"/product/{product.id}"
+        ) 
+        for product in response]
 
     return render_template("products_list.html.jinja", data=product_list)
+
+
+@bp_front_end.route('product/<int:product_id>', methods=['GET', 'POST'])
+def list_product_id(product_id):
+
+    if request.method == 'GET':
+        response = Product.query.filter_by(id=product_id)
+        product = [
+            dict(
+            id=product.id, 
+            name=product.name, 
+            image=product.image, 
+            price=product.price, 
+            category_id=product.category_id,
+            description=product.description
+            ) 
+            for product in response]
+
+        return render_template("product.html.jinja", data=product)
