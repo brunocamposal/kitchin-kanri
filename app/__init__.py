78d7d644e724eb6e6a8b483437766b1ca865728e
edu <eduardo.magno.u@gmail.com>
2021-01-20 21:26:45 -0300

    a

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = env.str('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
     app.config['JWT_SECRET_KEY'] = token_hex(16)
 
     # Configura db, ma e o migrate
